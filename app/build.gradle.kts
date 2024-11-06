eefe6e5caef8bc41e1f3a708b02ba659a8c4bde0
Oktay Genc <gncoktay3866@gmail.com>
2024-11-06 19:17:31 +0300

    chore: Update SDK to version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.oktaygenc.animatedsplashscreen"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.oktaygenc.animatedsplashscreen"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
